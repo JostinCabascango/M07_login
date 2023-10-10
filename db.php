1f07b4690f26c0ab6567f5d3bce5835976b4fe04
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-10 18:14:49 +0200

    Comprobamos si la conexion a la bd es correcta

--- db.php
+++ db.php
@@ -6,3 +6,12 @@ define('DB_USER', 'root');
 define('DB_PSW', '');
 // Puerto de conexion a la base de datos Mysql
 define('DB_PORT', '3306');
+// Hacemos la conexion a la base de datos
+$conexion = mysqli_connect(DB_HOST, DB_USER, DB_PSW, DB_NAME, DB_PORT);
+// Comprobamos si la conexion a la base de datos es correcta
+if (!$conexion) {
+    echo "La conexion a la base de datos ha fallado" . mysqli_connect_error();
+} else {
+    echo "Conexion realizada correctamente";
+}
+?>
