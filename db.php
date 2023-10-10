7e81733a5c1bfe454091dced717c6251c0e988dc
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-10 18:06:16 +0200

    Definida las constantes para hacer la conexion a la bd

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+//Constantes para la conexion a la base de datos MariaDB
+define('DB_HOST', 'localhost'); //127.0.0.1
+define('DB_NAME', 'users');
+define('DB_USER', 'root');
+define('DB_PSW', '');
+// Puerto de conexion a la base de datos Mysql
+define('DB_PORT', '3306');
