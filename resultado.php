bfad5ce0c6a9181ed1abbda834b43d854e925844
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-19 17:17:30 +0200

    Mostrar un mensaje si el insert se hace correctamente

--- resultado.php
+++ resultado.php
@@ -1,1 +1,10 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+</head>
+<body>
+<p>La inserció es correcte. <a href="login.html">Iniciar sessió usuari</a></p>
+</html>
