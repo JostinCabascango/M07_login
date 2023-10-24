fae914b8b198c1b16a1066c7d673fdc01d57952a
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-24 19:14:39 +0200

    Archivo .php que borra las variables de sesiones y destruye la sesion

--- php/desconectar.php
+++ php/desconectar.php
@@ -1,1 +1,19 @@
 <?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+</head>
+<body>
+<?php
+//Borrar totes les variables de sessió
+session_unset();
+//Destruir la sessió
+session_destroy();
+?>
+</body>
+</html>
+
