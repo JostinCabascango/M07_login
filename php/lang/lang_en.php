0a9d478e90b82811e66de0e91e226a1c7fe00262
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-11-02 11:13:26 +0100

    archivo con un array de las traduciones al ingles

---
+++ php/lang/lang_en.php
@@ -0,0 +1,23 @@
+<?php
+// Crear un array con las traducciones de los textos de la página a inglés
+$idioma=array(
+    'saludo' => 'Hello',
+    'es_profesor' => 'You are a teacher',
+    'es_alumno' => 'You are a student',
+    'mostrar_informacion' => 'Show information',
+    'desconectar' => 'Disconnect',
+    'eliminar' => 'Delete',
+    'nombre' => 'Name',
+    'apellido' => 'Surname',
+    'correo' => 'Email',
+    'accion' => 'Action',
+    'user_id' => 'User ID',
+    'name' => 'Name',
+    'surname' => 'Surname',
+    'password' => 'Password',
+    'email' => 'Email',
+    'rol' => 'Rol',
+    'h2'=>'Detailed user information',
+    'error'=>'No user ID received by GET',
+    'regresar'=>'Back',
+);
