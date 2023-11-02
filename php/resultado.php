9af4c31e61350d850415c304eac119f368ec7fb1
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-11-02 17:01:35 +0100

    Aeliminar codigo innecesario

--- php/resultado.php
+++ php/resultado.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado</title>
-    <!-- CSS de Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
+<?php
+$page_title = 'Resultado';
+include '../templates/header.php';
+?>
 <body>
 <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
     <div class="alert alert-success" role="alert">
@@ -14,4 +9,4 @@
     </div>
 </div>
 </body>
-</html>
+
